c4652e4485a3fef05faaf708bbfca612c8721c84
Yellsi <yellowm0098@gmail.com>
2021-03-25 23:02:10 +0200

    csv

--- webscrap.js
+++ webscrap.js
@@ -1,17 +1,22 @@
+const { fstat } = require('fs');
+const json2csv = require("json2csv")
 const puppeteer = require('puppeteer');
 const rp = require('request-promise');
 
+let mURL = "https://www.imdb.com/title/tt9784798/?ref_=adv_li_tt";
+rp(mURL)
+.then(function(html){
+//success
+
 (async () => {
-    let mURL = "https://www.imdb.com/title/tt9784798/?ref_=adv_li_tt";
+    
     
     let browser = await puppeteer.launch();
     let page = await browser.newPage();
-    rp(mURL)
-  .then(function(html){
-    //success
     await page.goto(mURL, {waitUntil: 'networkidle2'});
 
-    let movieData = await page.evaluate(() => {
+  
+    let movieData = page.evaluate(() => {
         let nameOfMovie = document.querySelector('div[class="title_wrapper"] > h1').innerText;
         let imdbRatings = document.querySelector('span[itemprop="ratingValue"]').innerText;
 
@@ -19,12 +24,21 @@ const rp = require('request-promise');
             nameOfMovie,
             imdbRatings
         }
+        movieData.push({
+            nameOfMovie : nameOfMovie,
+            imdbRatings: imdbRatings
+        }) 
     });
     console.log(movieData);
-    await browser.close();
     
- })
- .catch(function(err){
+    
+
+
+  await browser.close();
+}).catch(function(err){
     //handle error
   })
-})();
+  const j2cp = new json2csv()
+  const csv = j2cp.parse(movieData)
+  fstat.writeFileSync("./movie_scrap.csv", csv, "utf8");
+})
